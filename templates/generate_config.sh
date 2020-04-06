f4f85cc65490d2514d7299558b3ac3b4075976f0
Vincent LAURENT <vince.laurent@gmail.com>
2020-04-06 15:01:53 +0200

    Guillemet fermente pour la denière variable

--- templates/generate_config.sh
+++ templates/generate_config.sh
@@ -19,4 +19,4 @@ echo 'export creation_heures="'$fait_heures'"'
 echo 'export creation_minutes="'$fait_minutes'"'
 echo -n 'export motifs_join="'
 bash -c 'if test "$motif_travail" ; then echo -n "travail-"; fi ; if test "$motif_courses" ; then echo -n "courses-"; fi ;if test "$motif_sante" ; then echo -n "sante-"; fi ; if test "$motif_famille" ; then echo -n "famille-"; fi ; if test "$motif_sport" ; then echo -n "sport-"; fi ; if test "$motif_judiciaire" ; then echo -n "judiciaire-"; fi ; if test "$motif_missions" ; then echo -n "missions-"; fi ; ' | sed 's/-$/"/'
-echo 
+echo '"'
