4e9b4587d57aa923423c1e872ee9cf181fac8ebf
Tangui Morlier <tangui@tangui.eu.org>
2020-04-06 17:52:00 +0200

    Récupération automatique de la date et l'heure si rien n'est indiqué (suggestion de @0Llve sur twitter)

--- templates/generate_config.sh
+++ templates/generate_config.sh
@@ -9,11 +9,18 @@ echo -n "motif sante (x si vrai sinon vide) : " > /dev/stderr ; read motif_sante
 echo -n "motif famille (x si vrai sinon vide) : " > /dev/stderr ; read motif_famille ; echo 'export motif_famille="'$motif_famille'"'
 echo -n "motif sport (x si vrai sinon vide) : " > /dev/stderr ; read motif_sport ; echo 'export motif_sport="'$motif_sport'"'
 echo -n "motif judiciaire (x si vrai sinon vide) : " > /dev/stderr ; read motif_judiciaire ; echo 'export motif_judiciaire="'$motif_judiciaire'"'
-echo -n "motif missions (x si vrai sinon vide) : " > /dev/stderr ; read motif_missions ; echo 'export motif_missions="'$motif_missions'"'
+echo -n "motif missions (x si vrai sinon vide) : " > /dev/stderr ; read motif_missions ;  echo 'export motif_missions="'$motif_missions'"'
 echo -n "lieu de signature (même ville que adresse) : " > /dev/stderr ; read fait_lieu ; echo 'export fait_lieu="'$fait_lieu'"'
-echo -n "date de signature (JJ/MM/AAAA) : " > /dev/stderr ; read fait_date ; echo 'export fait_date="'$fait_date'"'
-echo -n "heure de signature (06 pour 6h45) : " > /dev/stderr ; read fait_heures ; echo 'export fait_heures="'$fait_heures'"'
-echo -n "minute de signature (45 pour 6h45) : " > /dev/stderr ; read fait_minutes ; echo 'export fait_minutes="'$fait_minutes'"'
+echo -n "date de signature (JJ/MM/AAAA) : " > /dev/stderr ; read fait_date ;
+if test ! "$fait_date"; then fait_date='`date +"%d/%m/%Y"`' ; fi
+echo 'export fait_date="'$fait_date'"'
+echo -n "heure de signature (06 pour 6h45) : " > /dev/stderr ; read fait_heures ;
+if ! test "$fait_heures"; then fait_heures='`date +"%H"`' ; fi
+echo 'export fait_heures="'$fait_heures'"'
+echo -n "minute de signature (45 pour 6h45) : " > /dev/stderr ; read fait_minutes ;
+if ! test "$fait_minutes"; then fait_minutes='`date +"%M"`' ; fi
+echo 'export fait_minutes="'$fait_minutes'"'
+echo 'export fait_minutes="'$fait_minutes'"'
 echo 'export creation_date="'$fait_date'"'
 echo 'export creation_heures="'$fait_heures'"'
 echo 'export creation_minutes="'$fait_minutes'"'
