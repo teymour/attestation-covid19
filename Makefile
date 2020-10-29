65bb08f2fe4959eb98d84fd28ead4d7b4545f8a2
Tangui Morlier <tangui@tangui.eu.org>
2020-10-29 22:16:40 +0100

    Motifications liées au QRCode

--- Makefile
+++ Makefile
@@ -32,7 +32,7 @@ $(build_path)/qr.png: $(build_path)/qr.txt
 	cat $(build_path)/qr.txt | $(qr_bin) > $(build_path)/qr.png
 
 $(build_path)/qr.txt: $(build_path)/config.inc templates/qr.txt.tmpl $(build_path)/.created
-	bash -c "source $(build_path)/config.inc ; cat templates/qr.txt.tmpl | tr -d '\n' | envsubst > $(build_path)/qr.txt"
+	bash -c "source $(build_path)/config.inc ; cat templates/qr.txt.tmpl | envsubst | tr -d '\n'  | sed 's/;/;\n/g' > $(build_path)/qr.txt"
 
 $(build_path)/config.inc: $(build_path)/.created $(config_file)
 	bash templates/config_avec_multimotifs.sh $(config_file) > $(build_path)/config.inc
@@ -51,7 +51,7 @@ cleanconfig:
 	rm $(config_file)
 
 test: generatetestfile
-	config_file=config/config_test.inc make -e realtest 
+	config_file=config/config_test.inc make -e realtest
 
 realtest: clean generatetestfile testqrcode testpages clean
 	rm -rf $(build_path)/* $(output_file) config/config_test.inc
@@ -60,8 +60,7 @@ realtest: clean generatetestfile testqrcode testpages clean
 generatetestfile: exemples/output.txt
 	cat exemples/output.txt | bash templates/generate_config.sh > config/config_test.inc 2> /dev/null
 
-testqrcode: $(build_path)/pdf_page-0.txt $(build_path)/pdf_page-1.txt $(build_path)/pdforiginal_page-0.txt $(build_path)/pdforiginal_page-1.txt
-	diff $(build_path)/pdf_page-0.txt $(build_path)/pdforiginal_page-0.txt && echo QRCODE page 1 OK
+testqrcode: $(build_path)/pdf_page-1.txt $(build_path)/pdforiginal_page-1.txt
 	diff $(build_path)/pdf_page-1.txt $(build_path)/pdforiginal_page-1.txt && echo QRCODE page 2 OK
 
 testpages: $(build_path)/diff_page-0.jpg $(build_path)/diff_page-1.jpg
--- templates/generate_config.sh
+++ templates/generate_config.sh
@@ -4,12 +4,14 @@ echo -n "date de naissance (JJ/MM/AAAA): " > /dev/stderr ; read naissance_date ;
 echo -n "lieu de naissance : " > /dev/stderr ; read naissance_lieu ; echo 'export naissance_lieu="'$naissance_lieu'"'
 echo -n "adresse (rue code_postal ville): " > /dev/stderr ; read adresse ; echo 'export adresse="'$adresse'"'
 echo -n "motif travail (x si vrai sinon vide): " > /dev/stderr ; read motif_travail ; echo 'export motif_travail="'$motif_travail'"'
-echo -n "motif courses (x si vrai sinon vide) : " > /dev/stderr ; read motif_courses ; echo 'export motif_courses="'$motif_courses'"'
+echo -n "motif achats (x si vrai sinon vide) : " > /dev/stderr ; read motif_courses ; echo 'export motif_courses="'$motif_courses'"'
 echo -n "motif sante (x si vrai sinon vide) : " > /dev/stderr ; read motif_sante ; echo 'export motif_sante="'$motif_sante'"'
 echo -n "motif famille (x si vrai sinon vide) : " > /dev/stderr ; read motif_famille ; echo 'export motif_famille="'$motif_famille'"'
-echo -n "motif sport (x si vrai sinon vide) : " > /dev/stderr ; read motif_sport ; echo 'export motif_sport="'$motif_sport'"'
+echo -n "motif handicap (x si vrai sinon vide) : " > /dev/stderr ; read motif_handicap ; echo 'export motif_handicap="'$motif_handicap'"'
+echo -n "motif sport_animaux (x si vrai sinon vide) : " > /dev/stderr ; read motif_sport ; echo 'export motif_sport="'$motif_sport'"'
 echo -n "motif judiciaire (x si vrai sinon vide) : " > /dev/stderr ; read motif_judiciaire ; echo 'export motif_judiciaire="'$motif_judiciaire'"'
 echo -n "motif missions (x si vrai sinon vide) : " > /dev/stderr ; read motif_missions ;  echo 'export motif_missions="'$motif_missions'"'
+echo -n "motif enfants (x si vrai sinon vide) : " > /dev/stderr ; read motif_enfants ;  echo 'export motif_enfants="'$motif_enfants'"'
 echo -n "lieu de signature (même ville que adresse) : " > /dev/stderr ; read fait_lieu ; echo 'export fait_lieu="'$fait_lieu'"'
 echo -n "date de signature (JJ/MM/AAAA, si vide la date courante sera utilisée) : " > /dev/stderr ; read fait_date ;
 if test ! "$fait_date"; then fait_date='`date +"%d/%m/%Y"`' ; fi
@@ -19,7 +21,6 @@ if ! test "$fait_heures"; then fait_heures='`date +"%H"`' ; fi
 echo 'export fait_heures="'$fait_heures'"'
 echo -n "minute de signature (45 pour 6h45, si vide l'heure courante sera utilisée) : " > /dev/stderr ; read fait_minutes ;
 if ! test "$fait_minutes"; then fait_minutes='`date +"%M"`' ; fi
-echo 'export fait_minutes="'$fait_minutes'"'
 echo 'export fait_minutes="'$fait_minutes'"'
 echo 'export creation_date="'$fait_date'"'
 echo 'export creation_heures="'$fait_heures'"'
